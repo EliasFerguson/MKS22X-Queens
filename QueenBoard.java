8ac4cbe3760e1d4ded74842b031346330b88ceff
Elias Ferguson <eferguson00@stuy.edu>
2019-02-07 10:08:24 -0500

    private methods temp public for testing purposes

--- QueenBoard.java
+++ QueenBoard.java
@@ -3,14 +3,14 @@ public class QueenBoard {
   public QueenBoard(int size) {
     board = new int[size][size];
   }
-  private boolean addQueen(int r, int c) {
+  public boolean addQueen(int r, int c) {
     if (board[r][c] == 0) {
       board[r][c] = -1;
       return true;
     }
     return false;
   }
-  private boolean removeQueen(int r, int c) {
+  public boolean removeQueen(int r, int c) {
     if (board[r][c] == -1) {
       board[r][c] = 0;
       return true;
