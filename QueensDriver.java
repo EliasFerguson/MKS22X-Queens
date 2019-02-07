52bc418a31d4d37d5e3958c26e0fd894ac6f630e
Elias Ferguson <eferguson00@stuy.edu>
2019-02-07 10:10:25 -0500

    deleted tests because the methods are back to private

--- QueensDriver.java
+++ QueensDriver.java
@@ -2,15 +2,5 @@ public class QueensDriver {
   public static void main(String[] args) {
     QueenBoard test = new QueenBoard(8);
     System.out.println(test.toString());
-    test.addQueen(1, 1);
-    System.out.println(test.toString());
-    test.removeQueen(1, 1);
-    System.out.println(test.toString());
-    test.removeQueen(1, 1);
-    System.out.println(test.toString());
-    test.addQueen(1, 1);
-    System.out.println(test.toString());
-    test.addQueen(1, 1);
-    System.out.println(test.toString());
   }
 }
