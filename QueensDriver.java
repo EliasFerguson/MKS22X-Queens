2e15983765402c3b923d5c6ba8da6b9b8b0eab51
Elias Ferguson <eferguson00@stuy.edu>
2019-02-08 12:13:12 -0500

    added test case for solve() of n = 8, should return true

--- QueensDriver.java
+++ QueensDriver.java
@@ -2,5 +2,7 @@ public class QueensDriver {
   public static void main(String[] args) {
     QueenBoard test = new QueenBoard(8);
     System.out.println(test.toString());
+    //Should print true;
+    System.out.println(test.solve());
   }
 }
